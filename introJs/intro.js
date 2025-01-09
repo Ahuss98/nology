435392c22e10c7b6f5de83b200248495e7eac799
Ahmed Hussain <a.hmedhussain@hotmail.com>
2025-01-09 14:16:44 +0000

    introJS

---
+++ introJs/intro.js
@@ -0,0 +1,11 @@
+var score = 10;
+var score = 10;
+console.log(++score);
+
+console.log(0 / 0 && 1 < 2);
+console.log(score || 1 > 2, 'XX');
+console.log(1 > 2 || score, 'XX2');
+console.log(1 < 2 && 0 / 0);
+console.log(5 < 6 && score);
+console.log(score && 5 < 6);
+console.log(parseInt('Roisin'));
