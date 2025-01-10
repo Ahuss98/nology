3ed2cdc0cee03bc77eb816915c5c893122c60094
Ahmed Hussain <a.hmedhussain@hotmail.com>
2025-01-10 09:36:22 +0000

    control flow

---
+++ control-flow-challenge-1/cf-challenge.js
@@ -0,0 +1,30 @@
+const day = 1;
+const message = 'Today is';
+
+switch (day) {
+	case 1:
+		console.log(`${message} Monday`);
+		break;
+	case 2:
+		console.log(`${message} Tuesday`);
+		break;
+	case 3:
+		console.log(`${message} Wednesday`);
+		break;
+	case 4:
+		console.log(`${message} Thursday`);
+		break;
+	case 5:
+		console.log(`${message} Friday`);
+		break;
+	case 6:
+		console.log(`${message} Saturday`);
+		break;
+	case 7:
+		console.log(`${message} sunday`);
+		break;
+	default:
+		console.log(
+			'Please pick a number between 1-7 to represent a day of the week from Monday - Sunday respectivley'
+		);
+}
